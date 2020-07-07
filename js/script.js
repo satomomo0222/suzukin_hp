4455ccefcf4e88f91bd4b7e22f55bda3334a5c25
satomomo0222 <satomomo0222@gmail.com>
2020-07-07 18:41:31 +0900

    add top

--- js/script.js
+++ js/script.js
@@ -3,3 +3,26 @@
   $(this).toggleClass('-open');
   $('.gnav').toggleClass('-open');
 });
+
+
+var video = document.getElementById('video');
+
+$('.sound-btn').on('click', function(){
+  $('.sound-btn i').toggleClass('fa-volume-mute');
+  $('.sound-btn i').toggleClass('fa-volume-up');
+  var video = document.getElementById('video');
+  if(video.muted){
+    video.muted = false;
+  }else{
+    video.muted = true;
+  }
+});
+
+// var mute = document.getElementById('mute');
+// mute.addEventListener('click', function () {
+//     if(video.muted){
+//         video.muted = false;
+//     }else{
+//         video.muted = true;
+//     }
+// });
